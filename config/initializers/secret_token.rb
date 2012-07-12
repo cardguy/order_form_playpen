d85a72141051736fa985f5924b1b5c05f2947d33
Rob McAleavy <rob@cardscreative.com>
2012-07-12 17:09:41 -0600

    Adding Rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OrderFormPlaypen::Application.config.secret_token = 'eef334b43072182e1fd46703f27b54052c50e5f697f07847ba3d0cd9346fd03504448a9788eda714a3e6f475bec00b0f3d1e0b0341be6b424652d36d44a853a0'
